03ca0deecbd0380b743742561e68c1f1e22aaf00
AaronCCWong <aarn.wong@gmail.com>
2019-05-16 23:46:50 -0400

    Refactor viterbi into its own class

---
+++ src/viterbi/Viterbi.java
@@ -0,0 +1,147 @@
+package viterbi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Viterbi {
+
+    Integer MAX_SUFFIX_LENGTH;
+
+    BigramModel model;
+    SuffixTree upperCaseTree;
+    SuffixTree lowerCaseTree;
+    List<String> tags;
+    Integer numTags;
+
+    public Viterbi(BigramModel bigramModel, SuffixTree upperCaseSuffixTree, SuffixTree lowerCaseSuffixTree, Integer maxSuffixLength) {
+        model = bigramModel;
+        upperCaseTree = upperCaseSuffixTree;
+        lowerCaseTree = lowerCaseSuffixTree;
+        MAX_SUFFIX_LENGTH = maxSuffixLength;
+
+        tags = model.getTags();
+        numTags = tags.size();
+    }
+
+    public List<String> run(List<String> sentence) {
+        Integer sentenceLength = sentence.size();
+        Matrix<Double> ppMatrix = new Matrix<Double>(numTags, sentenceLength);
+        Matrix<Integer> backpointer = new Matrix<Integer>(numTags, sentenceLength);
+
+        for (int state = 0; state < numTags; state++) {
+            Integer timeStep = 0;
+            Double prob = model.getStartProbability(tags.get(state));
+            String word = sentence.get(timeStep);
+            Double emissionProb = getEmissionProbability(state, word);
+            ppMatrix.set(state, timeStep, prob * emissionProb);
+            backpointer.set(state, timeStep, -1);
+        }
+
+        for (int timeStep = 1; timeStep < sentenceLength; timeStep++) {
+            String word = sentence.get(timeStep);
+            for (int state = 0; state < numTags; state++) {
+                Double maxProb = 0.0;
+                Integer maxPrevState = 0;
+                for (int prevState = 0; prevState < numTags; prevState++) {
+                    if (model.getTagTransitionCount(tags.get(prevState), tags.get(state)) > 0) {
+                        Double transitionProb = model.getTransitionProbability(tags.get(prevState), tags.get(state));
+                        Double prevProb = ppMatrix.get(prevState, timeStep - 1);
+                        if (maxProb < transitionProb * prevProb) {
+                            maxProb = transitionProb * prevProb;
+                            maxPrevState = prevState;
+                        }
+                    }
+                }
+
+                Double emissionProb = getEmissionProbability(state, word);
+                ppMatrix.set(state, timeStep, maxProb * emissionProb);
+                backpointer.set(state, timeStep, maxPrevState);
+            }
+        }
+
+        return getWordTags(ppMatrix, backpointer, sentenceLength);
+    }
+
+    private List<String> getWordTags(Matrix<Double> ppMatrix, Matrix<Integer> backpointer, Integer sentenceLength) {
+        Integer bestPathPointer = 0;
+        for (int state = 1; state < numTags; state++) {
+            Integer timeStep = sentenceLength - 1;
+            if (ppMatrix.get(state, timeStep) > ppMatrix.get(bestPathPointer, timeStep)) {
+                bestPathPointer = state;
+            }
+        }
+
+        int[] bestPath = new int[sentenceLength];
+        bestPath[sentenceLength - 1] = bestPathPointer;
+        for (int timeStep = sentenceLength - 2; timeStep >= 0; timeStep--) {
+            Integer nextTimeStep = timeStep + 1;
+            bestPath[timeStep] = backpointer.get(bestPath[nextTimeStep], nextTimeStep);
+        }
+
+        List<String> wordTags = new ArrayList<>();
+        for (int timeStep = 0; timeStep < sentenceLength; timeStep++) {
+            wordTags.add(tags.get(bestPath[timeStep]));
+        }
+
+        return wordTags;
+    }
+
+    private Double getEmissionProbability(Integer state, String word) {
+        if (model.getWordCount(word) > 0) {
+            return model.getEmissionProbability(tags.get(state), word);
+        }
+
+        Map<Integer, Double> stateProbs = getSuffixTagStats(word);
+        return stateProbs.get(state);
+    }
+
+    private Map<Integer, Double> getSuffixTagStats(String word) {
+        Integer numTags = tags.size();
+
+        Map<Integer, Double> stateProbs = new HashMap<>();
+        for (int state = 0; state < numTags; state++) {
+            String tag = tags.get(state);
+            Integer suffixLength = Math.min(MAX_SUFFIX_LENGTH, word.length());
+            String suffix = word.substring(word.length() - suffixLength);
+
+            Double tagSuffixProb = 0.0;
+            Double suffixProb = 0.0;
+            Double tagProb = 0.0;
+            if (Character.isUpperCase(word.charAt(0))) {
+                while (!upperCaseTree.hasSuffix(suffix) && suffix.length() > 0) {
+                    suffix = suffix.substring(1);
+                }
+                tagSuffixProb = upperCaseTree.getTagSuffixProbability(suffix, tag);
+                suffixProb = upperCaseTree.getSuffixProbability(suffix);
+                tagProb = upperCaseTree.getTagProbability(suffix, tag);
+            } else {
+                while (!lowerCaseTree.hasSuffix(suffix) && suffix.length() > 0) {
+                    suffix = suffix.substring(1);
+                }
+                tagSuffixProb = lowerCaseTree.getTagSuffixProbability(suffix, tag);
+                suffixProb = lowerCaseTree.getSuffixProbability(suffix);
+                tagProb = lowerCaseTree.getTagProbability(suffix, tag);
+            }
+
+            Double probWordIsTag = 0.0; // handles case when tag never occurs in the suffix tree
+            if (tagProb > 0.0) {
+                probWordIsTag = tagSuffixProb * suffixProb / tagProb;
+            }
+
+            stateProbs.put(state, probWordIsTag);
+        }
+
+        Map.Entry<Integer, Double> maxEntry = null;
+
+        for (Map.Entry<Integer, Double> entry : stateProbs.entrySet()) {
+            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+                maxEntry = entry;
+            }
+        }
+
+        return stateProbs;
+    }
+
+}
--- src/viterbi/WSJPOSTagger.java
+++ src/viterbi/WSJPOSTagger.java
@@ -4,11 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.Math;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class WSJPOSTagger {
@@ -90,91 +87,28 @@ public class WSJPOSTagger {
         Scanner sc = new Scanner(words);
         List<List<String>> sentenceTags = new ArrayList<>();
         List<List<String>> sentences = new ArrayList<>();
+        Viterbi viterbi = new Viterbi(model, upperCaseTree, lowerCaseTree, MAX_SUFFIX_LENGTH);
 
         List<String> currentSentence = new ArrayList<>();
-        Integer i = 0;
         while (sc.hasNextLine()) {
             String word = sc.nextLine();
 
             if (word.isEmpty()) {
-                sentenceTags.add(viterbi(model, upperCaseTree, lowerCaseTree, currentSentence));
+                sentenceTags.add(viterbi.run(currentSentence));
                 sentences.add(currentSentence);
                 currentSentence = new ArrayList<>();
             } else {
                 currentSentence.add(word);
             }
-
-            i++;
         }
 
         if (currentSentence.size() > 0) {
-            sentenceTags.add(viterbi(model, upperCaseTree, lowerCaseTree, currentSentence));
+            sentenceTags.add(viterbi.run(currentSentence));
             sentences.add(currentSentence);
         }
 
         sc.close();
         generateOutputFile(outputFilename, sentences, sentenceTags);
-    }
-
-    public static List<String> viterbi(BigramModel model, SuffixTree upperCaseTree, SuffixTree lowerCaseTree, List<String> sentence) {
-        List<String> tags = model.getTags();
-        Integer numTags = tags.size();
-        Integer sentenceLength = sentence.size();
-        Matrix<Double> ppMatrix = new Matrix<Double>(numTags, sentenceLength);
-        Matrix<Integer> backpointer = new Matrix<Integer>(numTags, sentenceLength);
-
-        for (int state = 0; state < numTags; state++) {
-            Integer timeStep = 0;
-            Double prob = model.getStartProbability(tags.get(state));
-            String word = sentence.get(timeStep);
-            Double emissionProb = getEmissionProbability(model, upperCaseTree, lowerCaseTree, tags, state, word);
-            ppMatrix.set(state, timeStep, prob * emissionProb);
-            backpointer.set(state, timeStep, -1);
-        }
-
-        for (int timeStep = 1; timeStep < sentenceLength; timeStep++) {
-            String word = sentence.get(timeStep);
-            for (int state = 0; state < numTags; state++) {
-                Double maxProb = 0.0;
-                Integer maxPrevState = 0;
-                for (int prevState = 0; prevState < numTags; prevState++) {
-                    if (model.getTagTransitionCount(tags.get(prevState), tags.get(state)) > 0) {
-                        Double transitionProb =  model.getTransitionProbability(tags.get(prevState), tags.get(state));
-                        Double prevProb = ppMatrix.get(prevState, timeStep - 1);
-                        if (maxProb < transitionProb * prevProb) {
-                            maxProb = transitionProb * prevProb;
-                            maxPrevState = prevState;
-                        }
-                    }
-                }
-
-                Double emissionProb = getEmissionProbability(model, upperCaseTree, lowerCaseTree, tags, state, word);
-                ppMatrix.set(state, timeStep, maxProb * emissionProb);
-                backpointer.set(state, timeStep, maxPrevState);
-            }
-        }
-
-        Integer bestPathPointer = 0;
-        for (int state = 1; state < numTags; state++) {
-            Integer timeStep = sentenceLength - 1;
-            if (ppMatrix.get(state, timeStep) > ppMatrix.get(bestPathPointer, timeStep)) {
-                bestPathPointer = state;
-            }
-        }
-
-        int[] bestPath = new int[sentenceLength];
-        bestPath[sentenceLength - 1] = bestPathPointer;
-        for (int timeStep = sentenceLength - 2; timeStep >= 0; timeStep--) {
-            Integer nextTimeStep = timeStep + 1;
-            bestPath[timeStep] = backpointer.get(bestPath[nextTimeStep], nextTimeStep);
-        }
-
-        List<String> taggedWords = new ArrayList<>();
-        for (int timeStep = 0; timeStep < sentenceLength; timeStep++) {
-            taggedWords.add(tags.get(bestPath[timeStep]));
-        }
-
-        return taggedWords;
     }
 
     public static void generateOutputFile(String filename, List<List<String>> sentences, List<List<String>> sentenceTags) throws IOException {
@@ -190,62 +124,6 @@ public class WSJPOSTagger {
         }
 
         writer.close();
-    }
-
-    public static Double getEmissionProbability(BigramModel model, SuffixTree upperCaseTree, SuffixTree lowerCaseTree, List<String> tags, Integer state, String word) {
-        if (model.getWordCount(word) > 0) {
-            return model.getEmissionProbability(tags.get(state), word);
-        }
-
-        Map<Integer, Double> stateProbs = getSuffixTagStats(upperCaseTree, lowerCaseTree, tags, word);
-        return stateProbs.get(state);
-    }
-
-    public static Map<Integer, Double> getSuffixTagStats(SuffixTree upperCaseTree, SuffixTree lowerCaseTree, List<String> tags, String word) {
-        Integer numTags = tags.size();
-
-        Map<Integer, Double> stateProbs = new HashMap<>();
-        for (int state = 0; state < numTags; state++) {
-            String tag = tags.get(state);
-            Integer suffixLength = Math.min(MAX_SUFFIX_LENGTH, word.length());
-            String suffix = word.substring(word.length() - suffixLength);
-
-            Double tagSuffixProb = 0.0;
-            Double suffixProb = 0.0;
-            Double tagProb = 0.0;
-            if (Character.isUpperCase(word.charAt(0))) {
-                while (!upperCaseTree.hasSuffix(suffix) && suffix.length() > 0) {
-                    suffix = suffix.substring(1);
-                }
-                tagSuffixProb = upperCaseTree.getTagSuffixProbability(suffix, tag);
-                suffixProb = upperCaseTree.getSuffixProbability(suffix);
-                tagProb = upperCaseTree.getTagProbability(suffix, tag);
-            } else {
-                while (!lowerCaseTree.hasSuffix(suffix) && suffix.length() > 0) {
-                    suffix = suffix.substring(1);
-                }
-                tagSuffixProb = lowerCaseTree.getTagSuffixProbability(suffix, tag);
-                suffixProb = lowerCaseTree.getSuffixProbability(suffix);
-                tagProb = lowerCaseTree.getTagProbability(suffix, tag);
-            }
-
-            Double probWordIsTag = 0.0; // handles case when tag never occurs in the suffix tree
-            if (tagProb > 0.0) {
-                probWordIsTag = tagSuffixProb * suffixProb / tagProb;
-            }
-
-            stateProbs.put(state, probWordIsTag);
-        }
-
-        Map.Entry<Integer, Double> maxEntry = null;
-
-        for (Map.Entry<Integer, Double> entry : stateProbs.entrySet()) {
-            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
-                maxEntry = entry;
-            }
-        }
-
-        return stateProbs;
     }
 
 }
